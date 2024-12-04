1abf2acf8e53d286621a7445c68da75dbd6cba6b
Phan Đăng Lưu <51915789+DieepjAnhr@users.noreply.github.com>
2024-12-04 21:33:29 +0700

    update

--- server/src/modules/blog/inputs/blog.input.ts
+++ server/src/modules/blog/inputs/blog.input.ts
@@ -1,32 +1,36 @@
-import { Field, ID, InputType, PartialType, OmitType } from '@nestjs/graphql';
+import {
+  Field,
+  ID,
+  InputType,
+  PartialType,
+  OmitType,
+} from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 import { Blog } from '../entities/blog.entity';
 
 @InputType()
-export class QueryBlogInput extends Blog {
-    @Field(() => String)
-    @IsNotEmpty()
-    title: string;
-
-    @Field(() => String)
-    @IsNotEmpty()
-    content: string;
-}
+export class QueryBlogInput extends Blog {}
 
 @InputType()
-export class CreateBlogInput extends OmitType(Blog, ['id', 'creator_id', 'created_at', 'updater_id', 'updated_at'] as const) {
-    @Field(() => String)
-    @IsNotEmpty()
-    title: string;
+export class CreateBlogInput extends OmitType(Blog, [
+  'id',
+  'creator_id',
+  'created_at',
+  'updater_id',
+  'updated_at',
+] as const) {
+  @Field(() => String)
+  @IsNotEmpty()
+  title: string;
 
-    @Field(() => String)
-    @IsNotEmpty()
-    content: string;
+  @Field(() => String)
+  @IsNotEmpty()
+  content: string;
 }
 
 @InputType()
 export class UpdateBlogInput extends PartialType(CreateBlogInput) {
-    @Field(() => ID)
-    @IsNotEmpty()
-    id: number;
+  @Field(() => ID)
+  @IsNotEmpty()
+  id: number;
 }
